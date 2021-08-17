53df4aba5496894019d37ebed3ab2dc9afbd1d03
3to5thpower <nisi3genki@gmail.com>
2021-08-18 03:56:33 +0900

    :sparkles: add domain user service

--- src/domain/service/user_service.rs
+++ src/domain/service/user_service.rs
@@ -1,5 +1,22 @@
 use crate::domain::model::user::User;
+use crate::domain::repository::user_repository::UserRepository;
 
-pub fn exists(user: User) -> bool {
-    true
+pub struct UserService<Repository: UserRepository> {
+    repository: Repository,
+}
+
+impl<Repository: UserRepository> UserService<Repository> {
+    pub fn exists(&self, user: User) -> bool {
+        match self.repository.get_user(user.id) {
+            Ok(_) => true,
+            Err(e) => {
+                let error_kind = e.root_cause().downcast_ref::<diesel::result::Error>();
+                if let Some(diesel::result::Error::NotFound) = error_kind {
+                    false
+                } else {
+                    panic!("Error: {}", e);
+                }
+            }
+        }
+    }
 }
